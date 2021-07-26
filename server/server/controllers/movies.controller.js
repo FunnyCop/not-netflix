7ddd98e66c7cd402a828f8b5f8e23ab652524b69
Johnathan Croft <croftmjohn@gmail.com>
2021-07-26 17:51:54 -0500

    create&update added

--- server/server/controllers/movies.controller.js
+++ server/server/controllers/movies.controller.js
@@ -14,13 +14,47 @@ module.exports.findAllMovies = (req,res) => {
 }
 
 module.exports.createMovie = (req,res)=>{
-    Movie.create(req.body)
-        .then(newMovie=>{
-            res.json({results: newMovie})
+    if (req.body.type === "Show" || req.body.type === "Movie"){
+        Movie.create(req.body)
+            .then(newMovie=>{
+                res.json({results: newMovie})
+            })
+            .catch(err => {
+                res.json(
+                    {message: "something went wrong creating a new movie", error: err}
+                    )
+            })
+    }
+    else {
+        res.json(
+            {message: "Type must be 'Show' or 'Movie' explicitely"}
+            )
+    }
+}
+
+module.exports.findOneMovie = (req, res)=>{
+    Movie.findOne({_id:req.params.movieId})
+        .then(OneMovie =>{
+            res.json({results: OneMovie})
         })
         .catch(err => {
             res.json(
-                {message: "something went wrong creating a new movie", error: err}
+                {message: "something went wrong finding one movie", error: err}
+                )
+        })
+}
+
+module.exports.updateMovie = (req, res)=>{
+    
+    Movie.findOneAndUpdate(
+        {_id: req.params.movieId},
+        req.body,
+        {new:true, runValidators:true}
+        )
+        .then(updatedMovie => res.json({results: updatedMovie}))
+        .catch(err => {
+            res.json(
+                {message: "something went wrong updating one movie", error: err}
                 )
         })
 }
