76ce12bca79027ba4758e07d2f0bcb1c24ca08df
Logan Prunty <lb_prunty@outlook.com>
2021-07-26 17:54:13 -0400

    setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import Home from "./views/Home.jsx"
+import { Router } from "@reach/router"
+
 import "./static/css/App.css"
 
 const App = () => {
   return (
     <div id = "App">
-
+      <Router>
+        <Home path = "/" />
+      </Router>
     </div>
   )
 }
