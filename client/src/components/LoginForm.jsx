f12853805c8bd9b318938822704f16257a8604ed
Logan Prunty <lb_prunty@outlook.com>
2021-07-26 18:53:16 -0400

    init

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "../static/css/LoginForm.css"
+
+const LoginForm = () => {
+    return (
+        <div id = "LoginForm"></div>
+    )
+}
+
+export default LoginForm
