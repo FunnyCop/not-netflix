a444bbe19ad5a5c2b66e076930e9694d3a953bd5
Raekian <Stevenzerwas@gmail.com>
2021-07-27 11:43:39 -0500

    init

---
+++ client/src/components/Featured.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+
+const Featured = () => {
+
+    return (
+        <div style={{ backgroundImage: "url(/mrrobot.jpeg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} className="featureBox">
+        </div>
+    )
+}
+
+
+export default Featured
