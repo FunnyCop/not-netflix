b59da1728fc36f136b2fba59cc4605aebecd25e9
Logan Prunty <lb_prunty@outlook.com>
2021-07-27 18:23:12 -0400

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,17 +17,11 @@ const App = () => {
   return (
     <div id="App">
       <Router>
-<<<<<<< HEAD
-        <Home path = "/" />
-        <Browse path = "/browse" />
-        <CredentialsTest path = "/test" />
-=======
         <Home path="/" />
         <Browse path="/browse" />
         <ProfileAdd path="/profile/add" />
         <ProfileEdit path="/profile/edit" />
         <ProfileImages path="/profile/picture/select" />
->>>>>>> ed17eab3d91c8d07dee711595af239a738db3f11
 
         {/* <LoginForm path = "/login" />
         <RegistrationForm path = "/register" /> */}
