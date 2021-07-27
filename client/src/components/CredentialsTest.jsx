e7965e83bcf431661d240c64fb4df10217d0a186
Logan Prunty <lb_prunty@outlook.com>
2021-07-27 19:40:09 -0400

    comments

--- client/src/components/CredentialsTest.jsx
+++ client/src/components/CredentialsTest.jsx
@@ -6,10 +6,14 @@ const CredentialsTest = () => {
     const [ user, setUser ] = useState( null )
 
     useEffect( () => {
+        // This only verifies that the cookie is signed by our secret
         axios.get( "http://localhost:8000/api/verify", { withCredentials: true } )
             .then( res => !res.data.message && navigate( "/" ) )
             .catch( () => navigate( "/" ) )
 
+        // This returns the user's document's _id
+        // I could rewrite this to verify the cookie and respond with the _id
+        // Since I'm using jwt.verify() anyway
         axios.get( "http://localhost:8000/api/user", { withCredentials: true } )
             .then( res => setUser( res.data._id ) )
             .catch( () => navigate( "/" ) )
