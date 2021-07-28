c3f6fd556f9c77b0fa87c99f730c201bb08c20c7
Raekian <Stevenzerwas@gmail.com>
2021-07-28 12:48:28 -0500

    Added Feature Component

--- client/src/components/Featured.js
+++ client/src/components/Featured.js
@@ -1,10 +1,23 @@
 import React, { useState } from 'react'
+import "../static/css/Featured.css"
 
 
 const Featured = () => {
 
     return (
-        <div style={{ backgroundImage: "url(/mrrobot.jpeg)", backgroundRepeat: 'no-repeat', backgroundSize: 'cover'}} className="featureBox">
+        <div>
+            <header class="v-header container">
+                <div class="fullscreen-video-wrap">
+                    <video autoPlay="true" loop="true" muted>
+                        <source src={"/video.mp4"} type='video/mp4' />
+                    </video>
+                </div>
+                <div class="header-content text-md-center">
+                    <h1>here are cars</h1>
+                    <p>a story about cars</p>
+                    <a class="btn btn-secondary">&#9655; play</a>
+                </div>
+            </header>
         </div>
     )
 }
--- client/src/views/Browse.jsx
+++ client/src/views/Browse.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 import { navigate } from "@reach/router"
 import NavBar from "../components/NavBar";
+import Featured from "../components/Featured";
 
 // This is just here to test cookie validation
 // Replace this with the actual page that should be displayed
@@ -18,6 +19,7 @@ const Browse = () => {
     return (
         <div id =  "Browse">
             <NavBar />
+            <Featured />
             Validation Successful
             </div>
     )
