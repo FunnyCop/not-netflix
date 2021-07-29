a1de570c7812a0ba58b039b962ea7fbe5775a3a0
Raekian <Stevenzerwas@gmail.com>
2021-07-29 10:33:40 -0500

    more fixes, my list changes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,45 +2,45 @@ import React from 'react'
 import axios from "axios"
 import { navigate } from "@reach/router"
 
-
 const NavBar = () => {
+
     const logout = () => {
-        axios.get( "http://localhost:8000/api/logout", { withCredentials: true } )
-            .then( () => navigate( "/" ) )
-            .catch( () => navigate( "/" ) )
+        axios.get("http://localhost:8000/api/logout", { withCredentials: true })
+            .then(() => navigate("/"))
+            .catch(() => navigate("/"))
     }
 
     return (
-            <nav id = "NavBar" className="navbar navbar-expand-lg navbar-dark ">
-                <div className="container-fluid">
+        <nav id="NavBar" className="navbar navbar-expand-lg navbar-dark ">
+            <div className="container-fluid">
                 <img className="logo" src={"/notnetflix.png"} alt="logo" />
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a id="navLinks" className="nav-link active" aria-current="page" href="/browse">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a id="navLinks" className="nav-link" href="/">TV Shows</a>
-                            </li>
-                            <li className="nav-item">
-                                <a id="navLinks" className="nav-link" href="/" aria-disabled="true">Movies</a>
-                            </li>
-                            <li className="nav-item">
-                                <a id="navLinks" className="nav-link" href="/" aria-disabled="true">New & Popular</a>
-                            </li>
-                            <li className="nav-item">
-                                <a id="navLinks" className="nav-link" href="/mylist" aria-disabled="true">My List</a>
-                            </li>
-                            <li className="nav-item">
-                                <a onClick = { logout } id="navLinks" className="nav-link" href="/" aria-disabled="true">Log Out</a>
-                            </li>
-                        </ul>
-                    </div>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <ul id="popOut" className="navbar-nav ml-auto mb-2 mb-lg-0 float-end text-end">
+                        <li className="nav-item">
+                            <a id="navLinks" className="nav-link active" aria-current="page" href="/browse">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="navLinks" className="nav-link" href="/">TV Shows</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="navLinks" className="nav-link" href="/" aria-disabled="true">Movies</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="navLinks" className="nav-link" href="/" aria-disabled="true">New & Popular</a>
+                        </li>
+                        <li className="nav-item">
+                            <a id="navLinks" className="nav-link" href="/mylist" aria-disabled="true">My List</a>
+                        </li>
+                        <li className="nav-item">
+                            <a onClick={logout} id="navLinks" className="nav-link" href="/" aria-disabled="true">Log Out</a>
+                        </li>
+                    </ul>
                 </div>
-            </nav>
+            </div>
+        </nav>
     )
 }
 
--- client/src/views/MyList.jsx
+++ client/src/views/MyList.jsx
@@ -6,30 +6,23 @@ import NavBar from "../components/NavBar";
 import "../static/css/MyList.css"
 
 const MyList = () => {
+    const[media, setMedia] = useState([]);
+    useEffect(()=>{
+        axios.get('http://localhost:8000/api/movies')
+            .then(res=>{
+                console.log(res)
+                setMedia(res.data.results);
+            });
+    }, [])
 
     return (
         <div>
             <NavBar />
         <div id="MyList">
             <div className="contentWrapper">
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
-                <div id="content"></div>
+            {media.map((item, idx)=>{
+                return <div id="content"><img id="mediaIMG"  src={`${item.image}`}/></div>
+            })}
             </div>
         </div>
         </div>
